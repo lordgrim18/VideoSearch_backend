b271606f1b6bd58eac0015e4f4962c3e3c07b661
Jerom <jeromjomanthara@gmail.com>
2024-07-20 14:20:49 +0530

    feat(db): add dynamoDB and client

--- src/config/dbConnection.js
+++ src/config/dbConnection.js
@@ -1,18 +1,15 @@
-const AWS = require("aws-sdk");
+var AWS = require("aws-sdk");
 require("aws-sdk/lib/maintenance_mode_message").suppress = true;
 const dotenv = require('dotenv').config();
 
-AWS.config.update({
+const dynamoDBConfig = {
     region: process.env.AWS_REGION,
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     endpoint: process.env.AWS_DDB_ENDPOINT
-  });
+};
 
-AWS.config.getCredentials(function(err) {
-  if (err) console.log(err.stack);
-  // credentials not loaded
-  else {
-    console.log("Access key:", AWS.config.credentials.accessKeyId);
-  }
-});
+const dynamoDB = new AWS.DynamoDB(dynamoDBConfig);
+const docClient = new AWS.DynamoDB.DocumentClient(dynamoDBConfig);
+
+module.exports = { dynamoDB, docClient };
